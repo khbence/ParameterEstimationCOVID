f6762e3d2be0e8a1505498d5d2ce716d1dea2ce4
khbence <horvathb94@gmail.com>
2020-07-16 16:26:50 +0200

    plot flag

--- parameterEstimationCovid.py
+++ parameterEstimationCovid.py
@@ -11,14 +11,16 @@ class ParameterEstimation:
     n : int
     N : int
     length : int
+    toPlot : bool
 
-    def __init__(self, refFile, n, N, length):
+    def __init__(self, refFile, n, N, length, toPlot):
         with open(refFile) as f:
             for line in f: # read lines
                 self.refData.append([float(i) for i in line.split()][0])
         self.n = n
         self.N = N
         self.length = length
+        self.toPlot = toPlot
 
     def __modifyConfigRandom(self, E):
         configRandomFile = open("inputs/config/in/configRandom.json.in", "rt")
@@ -43,19 +45,20 @@ class ParameterEstimation:
         agentOutput = os.popen('./covid -n {} -N {} -w {}{}{}'.format(self.n, self.N, self.length, sigmoidStr, files)).read()
         outDataFrame = pandas.read_csv(StringIO(agentOutput), sep='\t', index_col=False)
         ret =  outDataFrame[['I1', 'I2', 'I3', 'I4', 'I5', 'I6']].sum(axis = 1)
-        print("calculated: " + str(ret))
+#        print("calculated: " + str(ret))
         return ret.to_numpy() / self.n
     
     def objFunction(self, params):
         agent = self.__agentModel(params)
-        plt.plot(self.refData, label = 'ref')
-        plt.plot(agent, label = 'agent')
-        plt.show()
+        if(self.toPlot):
+            plt.plot(self.refData, label = 'ref')
+            plt.plot(agent, label = 'agent')
+            plt.show()
         ret = np.linalg.norm(agent - np.array(self.refData))
         return ret
 
 def main():
-    p = ParameterEstimation('inputs/deterministic_41_148.txt', 1000000, 1, 21)
+    p = ParameterEstimation('inputs/deterministic_41_148.txt', 1000000, 1, 21, False)
     params = OrderedDict()
     # params["E"] = ["real", (1e-7, 0.1)]
     params["m"] = ["real", (0.001, 0.3)]
@@ -65,7 +68,7 @@ def main():
 
     nm = NelderMead(p.objFunction, params)
     #nm.initialize([(1e-6, 0.125, 0.001, 0, 5), (1e-5, 0.125, 0.001, 0, 5), (1e-6, 0.13, 0.001, 0, 5), (1e-6, 0.125, 0.002, 0, 5), (1e-6, 0.125, 0.001, 0.1, 5), (1e-6, 0.125, 0.001, 0, 6)])
-    nm.initialize([(0.125, 0.001), (0.13, 0.001), (0.125, 0.0005)])
+    nm.initialize([(0.14457, 0.00228), (0.13, 0.001), (0.125, 0.0005)])
     nm.minimize()
 
 if __name__ == "__main__":
