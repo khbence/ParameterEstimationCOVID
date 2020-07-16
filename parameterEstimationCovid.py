e5783afb638e33d95627fd63c62e1fc5e6288dae
khbence <horvathb94@gmail.com>
2020-07-16 11:28:44 +0200

    runnable agent model and catched output

--- parameterEstimationCovid.py
+++ parameterEstimationCovid.py
@@ -2,34 +2,36 @@ import os
 import pandas
 from io import StringIO
 
-def modifyConfigRandom(E):
-    configRandomFile = open("inputs/config/in/configRandom.json.in", "rt")
-    data = configRandomFile.read()
-    data = data.replace('@S@', format(1.0 - E, '.20f'))
-    data = data.replace('@E@', format(E, '.20f'))
-    configRandomFile.close()
-    configRandomFileOut = open("inputs/config/out/configRandom.json", "wt")
-    configRandomFileOut.write(data)
-    configRandomFileOut.close()
+class ParameterEstimation:
+    def modifyConfigRandom(self, E):
+        configRandomFile = open("inputs/config/in/configRandom.json.in", "rt")
+        data = configRandomFile.read()
+        data = data.replace('@S@', format(1.0 - E, '.20f'))
+        data = data.replace('@E@', format(E, '.20f'))
+        configRandomFile.close()
+        configRandomFileOut = open("inputs/config/out/configRandom.json", "wt")
+        configRandomFileOut.write(data)
+        configRandomFileOut.close()
 
-def sigmoidParameters(m, v, H, s) -> str:
-    return ' -m {} -v {} -H {} -s {}'.format(m, v, H, s)
+    def sigmoidParameters(self, m, v, H, s) -> str:
+        return ' -m {} -v {} -H {} -s {}'.format(m, v, H, s)
 
-def filePaths() -> str:
-    return ' -P inputs/transition_final.json -A inputs/agentTypes.json -L inputs/locationTypes.json -p inputs/parameters.json -c inputs/config/out/configRandom.json'
+    def filePaths(self) -> str:
+        return ' -P inputs/transition_final.json -A inputs/agentTypes.json -L inputs/locationTypes.json -p inputs/parameters.json -c inputs/config/out/configRandom.json'
 
-def AgentModel(params, n, N, length) -> list:
-    modifyConfigRandom(params[0])
-    sigmoidStr = sigmoidParameters(params[1], params[2], params[3], params[4])
-    files = filePaths()
-    agentOutput = os.popen('./covid -n {} -N {} -w {}{}{}'.format(n, N, length, sigmoidStr, files)).read()
-    outDataFrame = pandas.read_csv(StringIO(agentOutput), sep='\t', index_col=False)
-    print(outDataFrame)
-    ret =  outDataFrame[['I1', 'I2', 'I3', 'I4', 'I5', 'I6']].sum(axis = 1)
-    return ret.tolist()
+    def AgentModel(self,params, n, N, length) -> list:
+        modifyConfigRandom(params[0])
+        sigmoidStr = sigmoidParameters(params[1], params[2], params[3], params[4])
+        files = filePaths()
+        agentOutput = os.popen('./covid -n {} -N {} -w {}{}{}'.format(n, N, length, sigmoidStr, files)).read()
+        outDataFrame = pandas.read_csv(StringIO(agentOutput), sep='\t', index_col=False)
+        print(outDataFrame)
+        ret =  outDataFrame[['I1', 'I2', 'I3', 'I4', 'I5', 'I6']].sum(axis = 1)
+        return ret.tolist()
 
 def main():
-    AgentModel([0.04, 0.125, 0.001, 0, 5], 1000, 1, 1)
+    ParameterEstimation p()
+    p.AgentModel([0.04, 0.125, 0.001, 0, 5], 1000, 1, 1)
 
 if __name__ == "__main__":
     main()
