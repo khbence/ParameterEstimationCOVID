a5284406f413f052739d157b776e004d00e401ed
khbence <horvathb94@gmail.com>
2020-07-16 13:45:53 +0200

    kinda working version

--- parameterEstimationCovid.py
+++ parameterEstimationCovid.py
@@ -1,9 +1,25 @@
 import os
 import pandas
 from io import StringIO
+import numpy as np
+from nelder_mead import NelderMead
+from collections import OrderedDict
 
 class ParameterEstimation:
-    def modifyConfigRandom(self, E):
+    refData = []
+    n : int
+    N : int
+    length : int
+
+    def __init__(self, refFile, n, N, length):
+        with open(refFile) as f:
+            for line in f: # read lines
+                self.refData.append([float(i) for i in line.split()][0])
+        self.n = n
+        self.N = N
+        self.length = length
+
+    def __modifyConfigRandom(self, E):
         configRandomFile = open("inputs/config/in/configRandom.json.in", "rt")
         data = configRandomFile.read()
         data = data.replace('@S@', format(1.0 - E, '.20f'))
@@ -13,25 +29,41 @@ class ParameterEstimation:
         configRandomFileOut.write(data)
         configRandomFileOut.close()
 
-    def sigmoidParameters(self, m, v, H, s) -> str:
+    def __sigmoidParameters(self, m, v, H, s) -> str:
         return ' -m {} -v {} -H {} -s {}'.format(m, v, H, s)
 
-    def filePaths(self) -> str:
+    def __filePaths(self) -> str:
         return ' -P inputs/transition_final.json -A inputs/agentTypes.json -L inputs/locationTypes.json -p inputs/parameters.json -c inputs/config/out/configRandom.json'
 
-    def AgentModel(self,params, n, N, length) -> list:
-        modifyConfigRandom(params[0])
-        sigmoidStr = sigmoidParameters(params[1], params[2], params[3], params[4])
-        files = filePaths()
-        agentOutput = os.popen('./covid -n {} -N {} -w {}{}{}'.format(n, N, length, sigmoidStr, files)).read()
+    def __agentModel(self, params) -> list:
+        print("Parameters: " + str(params))
+        self.__modifyConfigRandom(params[0])
+        sigmoidStr = self.__sigmoidParameters(params[1], params[2], params[3], params[4])
+        files = self.__filePaths()
+        # print('./covid -n {} -N {} -w {}{}{}'.format(self.n, self.N, self.length, sigmoidStr, files))
+        agentOutput = os.popen('./covid -n {} -N {} -w {}{}{}'.format(self.n, self.N, self.length, sigmoidStr, files)).read()
         outDataFrame = pandas.read_csv(StringIO(agentOutput), sep='\t', index_col=False)
-        print(outDataFrame)
         ret =  outDataFrame[['I1', 'I2', 'I3', 'I4', 'I5', 'I6']].sum(axis = 1)
-        return ret.tolist()
+        return ret.to_numpy() / self.n
+    
+    def objFunction(self, params):
+        ret = np.linalg.norm(self.__agentModel(params) - np.array(self.refData))
+        print("ERROR: " + str(ret))
+        return ret
 
 def main():
-    ParameterEstimation p()
-    p.AgentModel([0.04, 0.125, 0.001, 0, 5], 1000, 1, 1)
+    p = ParameterEstimation('inputs/deterministic_0_182.txt', 1000000, 1, 26)
+    params = OrderedDict()
+    params["E"] = ["real", (1e-8, 0.1)]
+    params["m"] = ["real", (0, 1)]
+    params["v"] = ["real", (0, 1)]
+    params["H"] = ["real", (0, 2)]
+    params["s"] = ["real", (0, 10)]
+
+    nm = NelderMead(p.objFunction, params)
+    nm.initialize([(1e-6, 0.125, 0.001, 0, 5), (1e-5, 0.125, 0.001, 0, 5), (1e-6, 0.13, 0.001, 0, 5), (1e-6, 0.125, 0.002, 0, 5), (1e-6, 0.125, 0.001, 0.1, 5), (1e-6, 0.125, 0.001, 0, 6)])
+
+    nm.minimize()
 
 if __name__ == "__main__":
     main()
