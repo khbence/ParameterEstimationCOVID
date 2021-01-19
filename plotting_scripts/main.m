7c9a471ba6a8c1b7fef803705c1ba4965bedab00
gerge <gergely.horvath96@gmail.com>
2021-01-19 13:51:36 +0100

    Readjustments have been made on the code to prepare it for the final format. Binary flags were included!

--- plotting_scripts/main.m
+++ plotting_scripts/main.m
@@ -1,41 +1,12 @@
-% A standard plotok előállításához a plotterpp(data, legend, title)
-% függvényt kell használni.
-
-% A data input egy string array, ami tartalmazza az elérési útvonalait a
-% scenarioknak, illetve a fájl neveket a sorszám előttig, a függvény
-% gyakorlatilag megkeresi az összes ilyen nevű fájlt és feldolgozza őket,
-% legyen akármennyi futtatás egy scenario-ra (sajnos azt még nem szereti,
-% ha csak 1 db futtatás van 1 scenario-ra). Fontos, hogy ezek txt-k
-% legyenek, és kizárólag az ágens számokat tartalmazzák, egyéb más
-% információkat (pl. iskola bezárás, záró sorok, stb.-t) ne, így kaptam az
-% adatokat, erre készítettem fel a beolvasó függvényt. Látható, hogy
-% fontos, hogy a fájlnevek a sorszámig, egyforma hosszúságúak legyenek, ez
-% gond nélkül megadható a futtató script-ben.
-
-% A legend input azonos dimenziójú string array, mint a data, ide azt kell
-% írni, hogy milyen néven akarod, hogy egyes scenariok a ploton
-% szerepeljenek. Ugyanazok a szabályok vonatkoznak rá, mint a data inputra,
-% hogy egyforma méretű elemek kellenek, ez white space-szel könnyen
-% megoldható.
-
-% A title input egy szem string, ez azt állítja be, hogy a plotokon felett
-% mi legyen a cím.
-
-% Csatoltam egy mappát benne pár példaadattal, ez a kód jelen állapotában
-% futtatható.
-
-% Jelenleg a többi inputját, aki futtattja, hagyja figyelmen kívül, majd a
-% teljes automatizáláshoz készültek flag-ek csak.
-
 close all; clear all; clc;
+
+% Jelenleg így dolgozható fel a standard output:
 
 % Data input megadása:
 
-sen1 = 'data/data0105/closureRules_from_jan5_nochange_';
-sen2 = 'data/data0105/closureRules_from_jan5_highschoolopennocurfew5open_';
-sen3 = 'data/data0105/closureRules_from_jan5_default_';
-
-Strings = {sen1;sen2;sen3};
+Strings = {'data/data0105/closureRules_from_jan5_nochange_',
+           'data/data0105/closureRules_from_jan5_highschoolopennocurfew5open_',
+           'data/data0105/closureRules_from_jan5_default_'};
 
 % Legend nevek megadása:
 
@@ -45,8 +16,8 @@ Scenarionames = {'Nov. 11-ei korlátozás nélkül',
              
 % Cím megadása:
              
-Title = 'Simulations starts from 23rd of September';
+Title = 'Valami cim';
 
 % Végül a függvény hívása:
 
-plotterpp(Strings,Scenarionames,Title,1,0)
+plotterpp(Strings,[],[],Scenarionames,Title,1,0,0)
--- plotting_scripts/plotterpp.m
+++ plotting_scripts/plotterpp.m
@@ -1,13 +1,5 @@
-function plotterpp(txtnames,scenarionames,Title,standard_flag,advanced_flag)
+function plotterpp(std_ip, agst_ip, tr_ip, scen_names, title, std_flag, agst_flag, tr_flag)
 
-    if standard_flag == 1 && advanced_flag == 0
-        plotter(txtnames,scenarionames,Title)
-    elseif standard_flag == 0 && advanced_flag == 1
-        fprintf("A funkcio jelenleg nem elerheto!\n")
-    elseif standard_flag == 1 && advanced_flag == 1
-        fprintf("A funkcio jelenleg nem elerheto!\n")
-    else
-        fprintf("Nem lehet mind a ketto flag nulla!\n")
-    end
+        plotter_std(std_ip,scen_names,title,std_flag)
 
 end
