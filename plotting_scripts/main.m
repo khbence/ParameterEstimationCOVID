3e5a8b62d9455f054677d6970e0dfc971d25a28d
gerge <gergely.horvath96@gmail.com>
2021-01-24 18:06:16 +0100

    main.m updated, tracing processing bug fix

--- plotting_scripts/main.m
+++ plotting_scripts/main.m
@@ -4,24 +4,22 @@ close all; clear all; clc;
 std_ip = {'data/dataexample/outputs_01_18/q1_',
           'data/dataexample/outputs_01_18/q2_',
           'data/dataexample/outputs_01_18/q3_'};
-
 % Agentstat input:     
 agst_ip = {'data/dataexample/outagent_01_18/outagent_q1_',
            'data/dataexample/outagent_01_18/outagent_q2_',
-           'data/dataexample/outagent_01_18/outagent_q3_'};
-       
+           'data/dataexample/outagent_01_18/outagent_q3_'};    
 % Trace input:
 tr_ip = {'data/dataexample/dumplist_01_18/dumplist_q1_',
          'data/dataexample/dumplist_01_18/dumplist_q2_',
          'data/dataexample/dumplist_01_18/dumplist_q3_'};
-
 % Legend nevek megadása:
 Scenarionames = {'q1',
                  'q2',
-                 'q3'};
-             
+                 'q3'};           
 % Cím megadása:
 Title = 'Valami cim';
 
 % Végül a függvény hívása:
 output = plotterpp(std_ip,agst_ip,tr_ip,Scenarionames,Title,1,1,1);
+
+clearvars agst_ip Scenarionames std_ip Title tr_ip
--- plotting_scripts/tracepostprocess.m
+++ plotting_scripts/tracepostprocess.m
@@ -25,17 +25,27 @@ function output = tracepostprocess(tr_ip,agst_ip,agents,locations)
     
     seged_av = zeros(max,M);
     seged_max = zeros(max,M);
+    seged_r0 = zeros(1,M);
+    seged_r0mod = zeros(1,M);
+    seged_eloszlas = cell(1,M);
     
     for i = 1 : N
         for j = 1 : M
             seged_av(:,j) = var(i).tracer(j).average;
             seged_max(:,j) = var(i).tracer(j).maximuminfections;
+            seged_r0(j) = var(i).r0fun(j).r0;
+            seged_r0mod(j) = var(i).r0fun(j).r0mod;
+            seged_eloszlas{1,j} = var(i).r0fun(j).eloszlas;
         end
         var2(i).tracer.average.av = mean(seged_av,2);
         var2(i).tracer.average.std = std(seged_av,0,2);
         var2(i).tracer.maximum.av = mean(seged_max,2);
         var2(i).tracer.maximum.std = std(seged_max,0,2);
-        var2(i).r0fun = var(i).r0fun;
+        var2(i).r0fun.eloszlas = seged_eloszlas;
+        var2(i).r0fun.r0.av = mean(seged_r0);
+        var2(i).r0fun.r0.std = std(seged_r0,0);
+        var2(i).r0fun.r0mod.av = mean(seged_r0mod);
+        var2(i).r0fun.r0mod.std = std(seged_r0mod,0);
     end
     
     output = var2;
