3749d8acb55a7c34a5cf120e42b6d2b6e12e9ce2
gerge <gergely.horvath96@gmail.com>
2021-01-24 17:14:23 +0100

    Trace postprocessing step, bug fix

--- plotting_scripts/tracepostprocess.m
+++ plotting_scripts/tracepostprocess.m
@@ -23,6 +23,21 @@ function output = tracepostprocess(tr_ip,agst_ip,agents,locations)
         end
     end
     
-    output = var;
+    seged_av = zeros(max,M);
+    seged_max = zeros(max,M);
+    
+    for i = 1 : N
+        for j = 1 : M
+            seged_av(:,j) = var(i).tracer(j).average;
+            seged_max(:,j) = var(i).tracer(j).maximuminfections;
+        end
+        var2(i).tracer.average.av = mean(seged_av,2);
+        var2(i).tracer.average.std = std(seged_av,0,2);
+        var2(i).tracer.maximum.av = mean(seged_max,2);
+        var2(i).tracer.maximum.std = std(seged_max,0,2);
+        var2(i).r0fun = var(i).r0fun;
+    end
+    
+    output = var2;
 
 end
