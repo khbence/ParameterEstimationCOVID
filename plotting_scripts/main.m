56e47ee5a4411141be7301eddce6e2def0c54c47
gerge <gergely.horvath96@gmail.com>
2021-01-24 17:25:03 +0100

    main.m updated

--- plotting_scripts/main.m
+++ plotting_scripts/main.m
@@ -1,38 +1,27 @@
 close all; clear all; clc;
 
-% Jelenleg így dolgozható fel a standard output:
-
-% Data input megadása:
-
 % Standard input:
-
 std_ip = {'data/dataexample/outputs_01_18/q1_',
           'data/dataexample/outputs_01_18/q2_',
           'data/dataexample/outputs_01_18/q3_'};
 
-% Agentstat input:
-      
+% Agentstat input:     
 agst_ip = {'data/dataexample/outagent_01_18/outagent_q1_',
            'data/dataexample/outagent_01_18/outagent_q2_',
            'data/dataexample/outagent_01_18/outagent_q3_'};
        
 % Trace input:
-
 tr_ip = {'data/dataexample/dumplist_01_18/dumplist_q1_',
          'data/dataexample/dumplist_01_18/dumplist_q2_',
          'data/dataexample/dumplist_01_18/dumplist_q3_'};
 
 % Legend nevek megadása:
-
 Scenarionames = {'q1',
                  'q2',
                  'q3'};
              
 % Cím megadása:
-
 Title = 'Valami cim';
 
 % Végül a függvény hívása:
-
 output = plotterpp(std_ip,agst_ip,tr_ip,Scenarionames,Title,1,1,1);
-
