a6d8cf617f7e6f3fc2510c45198bfaf03f0e5280
gerge <gergely.horvath96@gmail.com>
2021-02-05 07:46:36 +0100

    AgentStat bug fix

--- plotting_scripts/fun_plotter_agst.m
+++ plotting_scripts/fun_plotter_agst.m
@@ -1,169 +1,161 @@
 function fun_plotter_agst(jsonnames,scenarionames,Title,flag,agents,locations,clog,spectitle)
+        
+    scennum = length(scenarionames);
 
-    if flag == 1
-        
-        scennum = length(scenarionames);
-        
-        [data,agents,itbool] = fun_agst_organize(jsonnames,agents,locations,clog);
-        
-        if sum(itbool) ~= 0
-        
-            normnum = fun_agst_agentnormc(agents,scennum);
+    [data,agents,itbool] = fun_agst_organize(jsonnames,agents,locations,clog);
 
-            hossz = scennum;
-            indeces2bedeleted = [];
+    if sum(itbool) ~= 0
 
-            for i = 1 : hossz
-                if itbool(i) == 0
-                    indeces2bedeleted = [indeces2bedeleted i];
-                end
+        normnum = fun_agst_agentnormc(agents,scennum);
+
+        hossz = scennum;
+        indeces2bedeleted = [];
+
+        for i = 1 : hossz
+            if itbool(i) == 0
+                indeces2bedeleted = [indeces2bedeleted i];
             end
-
-            scenarionames(indeces2bedeleted) = [];
-
-            figure('Name','Plots1_agst','NumberTitle','off')
-
-            subplot(2,2,1)
-
-            bar_age = categorical({'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'});
-            bar_age = reordercats(bar_age,{'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'});
-            h = bar(bar_age,data.av.age);
-            set(h, {'DisplayName'}, scenarionames);
-            legend('Location','best');
-            xlabel('Age intervals [years]');
-            ylabel('Number of agents');
-            title('Age group distribution');
-
-            subplot(2,2,2)
-
-            bar_precond = categorical({'Healthy','Diabetes','Cardiovascular','Kidney','Obst. pulmonary'});
-            bar_precond = reordercats(bar_precond,{'Healthy','Diabetes','Cardiovascular','Kidney','Obst. pulmonary'});
-            h = bar(bar_precond,data.av.precond);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            xlabel('COVID-related chronic illnesses')
-            ylabel('Number of agents')
-            title('Precondition distribution')
-
-            subplot(2,2,3)
-
-            bar_age = categorical({'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'});
-            bar_age = reordercats(bar_age,{'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'});
-            h = bar(bar_age,data.av.age./normnum.ages);
-            set(h, {'DisplayName'}, scenarionames);
-            legend('Location','best');
-            xlabel('Age intervals [years]');
-            ylabel('Number of agents (normed)');
-            title('Age group distribution (normed with category size)');
-
-            subplot(2,2,4)
-
-            bar_precond = categorical({'Healthy','Diabetes','Cardiovascular','Kidney','Obst. pulmonary'});
-            bar_precond = reordercats(bar_precond,{'Healthy','Diabetes','Cardiovascular','Kidney','Obst. pulmonary'});
-            h = bar(bar_precond,data.av.precond./normnum.precond);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            xlabel('COVID-related chronic illnesses')
-            ylabel('Number of agents (normed)')
-            title('Precondition distribution (normed with category size)')
-
-            sgtitle(append(Title,' (Statistics-1 of the infected population) ',spectitle))
-
-            figure('Name','Plots2_agst','NumberTitle','off')
-
-            subplot(2,2,1)
-
-            bar_sex = categorical({'Female','Male'});
-            bar_sex = reordercats(bar_sex,{'Female','Male'});
-            h = bar(bar_sex,data.av.sex);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            ylabel('Number of agents')
-            title('Sex distribution')
-
-            subplot(2,2,2)
-
-            bar_worststate = categorical({'Exposed','Presymptomatic','Asymptomatic','Light symptomatic','Mild symptomatic','Hospitalized','Intensive care','Deceased'});
-            bar_worststate = reordercats(bar_worststate,{'Exposed','Presymptomatic','Asymptomatic','Light symptomatic','Mild symptomatic','Hospitalized','Intensive care','Deceased'});
-            var = data.av.worststate;
-            var(:,10) = var(:,1);
-            var(:,1:2) = [];
-            h = bar(bar_worststate,var);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            ylabel('Number of agents')
-            set(gca,'YScale','log')
-            title('Worst state distribution')
-
-            subplot(2,2,3)
-
-            bar_infections = categorical({'Infected','Not infected'});
-            bar_infections = reordercats(bar_infections,{'Infected','Not infected'});
-            h = bar(bar_infections,data.av.infections);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            ylabel('Number of agents')
-            set(gca,'YScale','log')
-            title('Infected - Not infected agents')
-
-            subplot(2,2,4)
-
-            bar_diagnosises = categorical({'Diagnosed','Not diagnosed'});
-            bar_diagnosises = reordercats(bar_diagnosises,{'Diagnosed','Not diagnosed'});
-            h = bar(bar_diagnosises,data.av.diagnoses);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            ylabel('Number of agents')
-            title('Diagnosed - Not diagnosed agents')
-
-            sgtitle(append(Title,' (Statistics-2 of the infected population) ',spectitle))
-
-            figure('Name','Plots3_agst','NumberTitle','off')
-
-            subplot(2,2,1)
-
-            bar_agenttype = categorical({'Infant','Kindergarten student','Elementary school student','High school student','University student','Full-time (standard, fixed)','Afternoon shift worker','Stay-at-home schedule','Tourist'});
-            bar_agenttype = reordercats(bar_agenttype,{'Infant','Kindergarten student','Elementary school student','High school student','University student','Full-time (standard, fixed)','Afternoon shift worker','Stay-at-home schedule','Tourist'});
-            h = bar(bar_agenttype,data.av.agenttype);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            ylabel('Number of agents')
-            set(gca,'YScale','log')
-            title('Agent type distribution')
-
-            subplot(2,2,2)
-
-            bar_agenttype = categorical({'Infant','Kindergarten student','Elementary school student','High school student','University student','Full-time (standard, fixed)','Afternoon shift worker','Stay-at-home schedule','Tourist'});
-            bar_agenttype = reordercats(bar_agenttype,{'Infant','Kindergarten student','Elementary school student','High school student','University student','Full-time (standard, fixed)','Afternoon shift worker','Stay-at-home schedule','Tourist'});
-            h = bar(bar_agenttype,data.av.agenttype./normnum.typeID);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            ylabel('Number of agents (normed)')
-            set(gca,'YScale','log')
-            title('Agent type distribution (normed with category size)')
-
-            subplot(2,2,[3 4])
-
-            bar_locationtype = categorical({'Public spaces','Residence','Public education','Std., full-time wp.','Small social site','Large social site','Short visiting site','Long visiting site','Weekend social sites','Recreational sites','Closed facility','Hospital','Non-std. schedule wp.','Health center','Commuters','Nursery home','Classroom','University'});
-            bar_locationtype = reordercats(bar_locationtype,{'Public spaces','Residence','Public education','Std., full-time wp.','Small social site','Large social site','Short visiting site','Long visiting site','Weekend social sites','Recreational sites','Closed facility','Hospital','Non-std. schedule wp.','Health center','Commuters','Nursery home','Classroom','University'});
-            h = bar(bar_locationtype,data.av.locationtype);
-            set(h, {'DisplayName'}, scenarionames)
-            legend('Location','best')
-            ylabel('Number of agents')
-            set(gca,'YScale','log')
-            title('Infection locations')
-
-            sgtitle(append(Title,' (Statistics-3 of the infected population) ',spectitle))
-
-            fprintf(append("Agent statistics input successfully processed! (",spectitle,")\n"))
-        else
-            fprintf(append('Agent statistics cannot be processed for the ',spectitle,', because there were no agents to start with!\n'));
         end
-            
-    elseif flag == 0
-        fprintf("Agentstat input processing was not requested!\n")
+
+        scenarionames(indeces2bedeleted) = [];
+
+        figure('Name','Plots1_agst','NumberTitle','off')
+
+        subplot(2,2,1)
+
+        bar_age = categorical({'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'});
+        bar_age = reordercats(bar_age,{'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'});
+        h = bar(bar_age,data.av.age);
+        set(h, {'DisplayName'}, scenarionames);
+        legend('Location','best');
+        xlabel('Age intervals [years]');
+        ylabel('Number of agents');
+        title('Age group distribution');
+
+        subplot(2,2,2)
+
+        bar_precond = categorical({'Healthy','Diabetes','Cardiovascular','Kidney','Obst. pulmonary'});
+        bar_precond = reordercats(bar_precond,{'Healthy','Diabetes','Cardiovascular','Kidney','Obst. pulmonary'});
+        h = bar(bar_precond,data.av.precond);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        xlabel('COVID-related chronic illnesses')
+        ylabel('Number of agents')
+        title('Precondition distribution')
+
+        subplot(2,2,3)
+
+        bar_age = categorical({'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'});
+        bar_age = reordercats(bar_age,{'0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99'});
+        h = bar(bar_age,data.av.age./normnum.ages);
+        set(h, {'DisplayName'}, scenarionames);
+        legend('Location','best');
+        xlabel('Age intervals [years]');
+        ylabel('Number of agents (normed)');
+        title('Age group distribution (normed with category size)');
+
+        subplot(2,2,4)
+
+        bar_precond = categorical({'Healthy','Diabetes','Cardiovascular','Kidney','Obst. pulmonary'});
+        bar_precond = reordercats(bar_precond,{'Healthy','Diabetes','Cardiovascular','Kidney','Obst. pulmonary'});
+        h = bar(bar_precond,data.av.precond./normnum.precond);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        xlabel('COVID-related chronic illnesses')
+        ylabel('Number of agents (normed)')
+        title('Precondition distribution (normed with category size)')
+
+        sgtitle(append(Title,' (Statistics-1 of the infected population) ',spectitle))
+
+        figure('Name','Plots2_agst','NumberTitle','off')
+
+        subplot(2,2,1)
+
+        bar_sex = categorical({'Female','Male'});
+        bar_sex = reordercats(bar_sex,{'Female','Male'});
+        h = bar(bar_sex,data.av.sex);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        ylabel('Number of agents')
+        title('Sex distribution')
+
+        subplot(2,2,2)
+
+        bar_worststate = categorical({'Exposed','Presymptomatic','Asymptomatic','Light symptomatic','Mild symptomatic','Hospitalized','Intensive care','Deceased'});
+        bar_worststate = reordercats(bar_worststate,{'Exposed','Presymptomatic','Asymptomatic','Light symptomatic','Mild symptomatic','Hospitalized','Intensive care','Deceased'});
+        var = data.av.worststate;
+        var(:,10) = var(:,1);
+        var(:,1:2) = [];
+        h = bar(bar_worststate,var);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        ylabel('Number of agents')
+        set(gca,'YScale','log')
+        title('Worst state distribution')
+
+        subplot(2,2,3)
+
+        bar_infections = categorical({'Infected','Not infected'});
+        bar_infections = reordercats(bar_infections,{'Infected','Not infected'});
+        h = bar(bar_infections,data.av.infections);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        ylabel('Number of agents')
+        set(gca,'YScale','log')
+        title('Infected - Not infected agents')
+
+        subplot(2,2,4)
+
+        bar_diagnosises = categorical({'Diagnosed','Not diagnosed'});
+        bar_diagnosises = reordercats(bar_diagnosises,{'Diagnosed','Not diagnosed'});
+        h = bar(bar_diagnosises,data.av.diagnoses);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        ylabel('Number of agents')
+        title('Diagnosed - Not diagnosed agents')
+
+        sgtitle(append(Title,' (Statistics-2 of the infected population) ',spectitle))
+
+        figure('Name','Plots3_agst','NumberTitle','off')
+
+        subplot(2,2,1)
+
+        bar_agenttype = categorical({'Infant','Kindergarten student','Elementary school student','High school student','University student','Full-time (standard, fixed)','Afternoon shift worker','Stay-at-home schedule','Tourist'});
+        bar_agenttype = reordercats(bar_agenttype,{'Infant','Kindergarten student','Elementary school student','High school student','University student','Full-time (standard, fixed)','Afternoon shift worker','Stay-at-home schedule','Tourist'});
+        h = bar(bar_agenttype,data.av.agenttype);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        ylabel('Number of agents')
+        set(gca,'YScale','log')
+        title('Agent type distribution')
+
+        subplot(2,2,2)
+
+        bar_agenttype = categorical({'Infant','Kindergarten student','Elementary school student','High school student','University student','Full-time (standard, fixed)','Afternoon shift worker','Stay-at-home schedule','Tourist'});
+        bar_agenttype = reordercats(bar_agenttype,{'Infant','Kindergarten student','Elementary school student','High school student','University student','Full-time (standard, fixed)','Afternoon shift worker','Stay-at-home schedule','Tourist'});
+        h = bar(bar_agenttype,data.av.agenttype./normnum.typeID);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        ylabel('Number of agents (normed)')
+        set(gca,'YScale','log')
+        title('Agent type distribution (normed with category size)')
+
+        subplot(2,2,[3 4])
+
+        bar_locationtype = categorical({'Public spaces','Residence','Public education','Std., full-time wp.','Small social site','Large social site','Short visiting site','Long visiting site','Weekend social sites','Recreational sites','Closed facility','Hospital','Non-std. schedule wp.','Health center','Commuters','Nursery home','Classroom','University'});
+        bar_locationtype = reordercats(bar_locationtype,{'Public spaces','Residence','Public education','Std., full-time wp.','Small social site','Large social site','Short visiting site','Long visiting site','Weekend social sites','Recreational sites','Closed facility','Hospital','Non-std. schedule wp.','Health center','Commuters','Nursery home','Classroom','University'});
+        h = bar(bar_locationtype,data.av.locationtype);
+        set(h, {'DisplayName'}, scenarionames)
+        legend('Location','best')
+        ylabel('Number of agents')
+        set(gca,'YScale','log')
+        title('Infection locations')
+
+        sgtitle(append(Title,' (Statistics-3 of the infected population) ',spectitle))
+
+        fprintf(append("Agent statistics input successfully processed! (",spectitle,")\n"))
     else
-        fprintf("You used something other than 0 or 1 for a binary flag!\n")
+        fprintf(append('Agent statistics cannot be processed for the ',spectitle,', because there were no agents to start with!\n'));
     end
     
 end
--- plotting_scripts/fun_plotter_agstorg.m
+++ plotting_scripts/fun_plotter_agstorg.m
@@ -1,13 +1,21 @@
 function fun_plotter_agstorg(agst_ip,scen_names,Title,agst_flag)
 
-    locations = jsondecode(fileread('locations0.json'));
-    locations = locations.places;
-    agents = jsondecode(fileread('agents0.json'));
-    agents = agents.people;
+    if agst_flag == 1
+        
+        locations = jsondecode(fileread('locations0.json'));
+        locations = locations.places;
+        agents = jsondecode(fileread('agents0.json'));
+        agents = agents.people;
 
-    fun_plotter_agst(agst_ip,scen_names,Title,agst_flag,agents,locations,0,'general')
-    fun_plotter_agst(agst_ip,scen_names,Title,agst_flag,agents,locations,1,'deceased portion')
-    fun_plotter_agst(agst_ip,scen_names,Title,agst_flag,agents,locations,2,'quarantined portion')
-    fun_plotter_agst(agst_ip,scen_names,Title,agst_flag,agents,locations,3,'pensioner portion')
+        fun_plotter_agst(agst_ip,scen_names,Title,agst_flag,agents,locations,0,'general')
+        fun_plotter_agst(agst_ip,scen_names,Title,agst_flag,agents,locations,1,'deceased portion')
+        fun_plotter_agst(agst_ip,scen_names,Title,agst_flag,agents,locations,2,'quarantined portion')
+        fun_plotter_agst(agst_ip,scen_names,Title,agst_flag,agents,locations,3,'pensioner portion')
+    
+    elseif agst_flag == 0
+        fprintf("Agentstat input processing was not requested!\n")
+    else
+        fprintf("You used something other than 0 or 1 for a binary flag!\n")
+    end
 
 end
