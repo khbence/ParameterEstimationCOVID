8f9fd3fa5c0f3c42039791aca2dcadf3b713a5a7
gerge <gergely.horvath96@gmail.com>
2021-02-20 22:30:40 +0100

    Infection events distribution figure

--- plotting_scripts/fun_plotter_tr.m
+++ plotting_scripts/fun_plotter_tr.m
@@ -65,6 +65,23 @@ function output = fun_plotter_tr(tr_ip,agst_ip,scenarionames,Title,Measures,Star
         legend('Location','best')
         
         sgtitle(append('Infection tracing time series (',Title,')'))
+        
+        for i = 1 : length(output)
+            figure('Name',append('Plots',num2str(i+1),'_tr'),'NumberTitle','off')
+            histogram('BinCounts',output(i).r0fun.eloszlas,'BinEdges',output(i).r0fun.eloszlasX)
+            grid on
+            grid minor
+            xlabel('No. of infection events')
+            ylabel('Counts')
+            set(gca,'YScale','log')
+            title(append('Mean infection event distribution of the ',scenarionames{i},' scenario'))
+        end
+        
+        output = output.r0fun;
+        atadas = output;
+        output = [];
+        output.r0 = atadas.r0;
+        output.r0mod = atadas.r0mod;
 
         fprintf(append("Contact tracing input successfully processed!\n"))
         
