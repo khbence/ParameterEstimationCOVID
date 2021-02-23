980f352b6e5370dbb64632e3a0ca976c991bde4b
gerge <gergely.horvath96@gmail.com>
2021-02-23 16:15:47 +0100

    Update for extensive testing

--- plotting_scripts/main.m
+++ plotting_scripts/main.m
@@ -2,31 +2,27 @@ close all; clear all; clc;
 
 % Standard input:
 std_ip = {'data/article/default/default_',
-          'data/article/testing_probability/testing_probability_10_times_more_random_',
-          'data/article/testing_probability/testing_probability_10_times_more_home_',
-          'data/article/testing_probability/testing_probability_10_times_more_workschool_',
-          'data/article/testing_probability/testing_probability_10_times_more_hospital_nursing_home_'};
+          'data/article/closures_school_level/school_closes_',
+          'data/article/closures_school_level/no_school_'};
 % Agentstat input:
 agst_ip = {'data/article/default/outAgent/default_outagent_',
-           'data/article/testing_probability/outAgent/testing_probability_10_times_more_random_outagent_',
-           'data/article/testing_probability/outAgent/testing_probability_10_times_more_home_outagent_',
-           'data/article/testing_probability/outAgent/testing_probability_10_times_more_workschool_outagent_',
-           'data/article/testing_probability/outAgent/testing_probability_10_times_more_hospital_nursing_home_outagent_'};
+           'data/article/closures_school_level/outAgent/school_closes_outagent_',
+           'data/article/closures_school_level/outAgent/no_school_outagent_'};
 % Trace input:
 tr_ip = {'data/data0203/dumplist_02_03/dumplist_'};
 % Legend nevek megadása:
-Scenarionames = {'Default';'Random';'Home';'Work, school';'Hospital, nursery'};
+Scenarionames = {'Close infected classes',' Close infected schools','Close all schools'}; % Close infected classes: default
 % Cím megadása:
-Title = 'More testing prob. simulations';
+Title = 'Educational simulations';
 % Plot határok bejelölése:
 Measures = {{'Restr.'},49;
             {'Vacci.'},96};
 % Kezdődátum megadása:
 StartDate = '09/23/20'; % mm/dd/yy
 % Mentett grafikonok helye:
-Path = 'data/plots/publication/testing_probability';
+Path = 'data/plots/publication/closures_school_level_repro';
 
 % Végül a függvény hívása:
-output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scenarionames,Title,Measures,StartDate,1,1,0,0,Path);
+output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scenarionames,Title,Measures,StartDate,1,0,0,0,Path);
 
 clearvars agst_ip Scenarionames std_ip Title tr_ip Measures StartDate Path
