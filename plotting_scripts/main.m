0f2f8d9a1b56a93f75a31f48a8708fd4ce4ce61a
gerge <gergely.horvath96@gmail.com>
2021-02-23 11:56:31 +0100

    Update for extensive testing

--- plotting_scripts/main.m
+++ plotting_scripts/main.m
@@ -1,26 +1,30 @@
 close all; clear all; clc;
 
 % Standard input:
-std_ip = {'data/article/vaccination_amount/vaccination_amount_005_',
-          'data/article/default/default_',
-          'data/article/vaccination_amount/vaccination_amount_02_'};
+std_ip = {'data/article/default/default_',
+          'data/article/testing_probability/testing_probability_10_times_more_random_',
+          'data/article/testing_probability/testing_probability_10_times_more_home_',
+          'data/article/testing_probability/testing_probability_10_times_more_workschool_',
+          'data/article/testing_probability/testing_probability_10_times_more_hospital_nursing_home_'};
 % Agentstat input:
-agst_ip = {'data/article/vaccination_amount/outAgent/vaccination_amount_005_outagent_',
-           'data/article/default/outAgent/default_outagent_',
-           'data/article/vaccination_amount/outAgent/vaccination_amount_02_outagent_'};
+agst_ip = {'data/article/default/outAgent/default_outagent_',
+           'data/article/testing_probability/outAgent/testing_probability_10_times_more_random_outagent_',
+           'data/article/testing_probability/outAgent/testing_probability_10_times_more_home_outagent_',
+           'data/article/testing_probability/outAgent/testing_probability_10_times_more_workschool_outagent_',
+           'data/article/testing_probability/outAgent/testing_probability_10_times_more_hospital_nursing_home_outagent_'};
 % Trace input:
 tr_ip = {'data/data0203/dumplist_02_03/dumplist_'};
 % Legend nevek megadása:
-Scenarionames = {'0.05%','0.1%','0.2%'}';
+Scenarionames = {'Default';'Random';'Home';'Work, school';'Hospital, nursery'};
 % Cím megadása:
-Title = 'Vaccination ammount simulations';
+Title = 'More testing prob. simulations';
 % Plot határok bejelölése:
 Measures = {{'Restr.'},49;
             {'Vacci.'},96};
 % Kezdődátum megadása:
 StartDate = '09/23/20'; % mm/dd/yy
 % Mentett grafikonok helye:
-Path = 'data/plots/publication/vaccination_amount';
+Path = 'data/plots/publication/testing_probability';
 
 % Végül a függvény hívása:
 output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scenarionames,Title,Measures,StartDate,1,1,0,0,Path);
