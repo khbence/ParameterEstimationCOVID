1125cbe34e413baf107c73604fafc4d9eba1c25d
gerge <gergely.horvath96@gmail.com>
2021-02-28 12:37:42 +0100

    main.m bug fix

--- plotting_scripts/main.m
+++ plotting_scripts/main.m
@@ -2,13 +2,13 @@ close all; clear all; clc;
 % Standard input:
 std_ip = {'data/fullRerun/default/default_',
           'data/fullRerun/closures_school_levels/school_closes_',
-          'data/fullRerun/closures_school_levels/no_school_'};
+          'data/fullRerun/closures_school_levels/no_school_'}; std_flag = 1;
 % Agentstat input:
 agst_ip = {'data/fullRerun/default/outAgent/default_outagent_',
            'data/fullRerun/closures_school_levels/outAgent/school_closes_outagent_',
-           'data/fullRerun/closures_school_levels/outAgent/no_school_outagent_'};
+           'data/fullRerun/closures_school_levels/outAgent/no_school_outagent_'}; agst_flag = 0;
 % Trace input:
-tr_ip = {'data/data0203/dumplist_02_03/dumplist_'};
+tr_ip = {'data/data0203/dumplist_02_03/dumplist_'}; tr_flag = 0;
 % Legend nevek megadása:
 Scennames = {'Close infected classes';'Close infected schools';'Close all schools'};
 % Cím megadása:
@@ -17,11 +17,11 @@ Title = 'Educational simulations';
 Measures = {{'Restr.'},49;
             {'Vacci.'},96};
 % Kezdődátum megadása:
-StartDate = '09/23/20'; % mm/dd/yy
+StartD = '09/23/20'; % mm/dd/yy
 % Mentett grafikonok helye:
 Path = 'data/attempt/schooltest4';
-
+% Mérési adat flag:
+m_flag = 0;
 % Végül a függvény hívása:
-output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scennames,Title,Measures,StartDate,1,0,0,0,Path);
-
-clearvars agst_ip Scennames std_ip Title tr_ip Measures StartDate Path VaccinationConstants
+output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scennames,Title,Measures,StartD,std_flag,agst_flag,tr_flag,m_flag,Path);
+clearvars agst_ip Scennames std_ip Title tr_ip Measures StartD Path std_flag agst_flag tr_flag m_flag
