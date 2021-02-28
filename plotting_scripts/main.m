a74c8bf066fb90aa3aa34e3c4bcb819a11c40bd7
gerge <gergely.horvath96@gmail.com>
2021-02-28 16:13:36 +0100

    main.m update

--- plotting_scripts/main.m
+++ plotting_scripts/main.m
@@ -12,16 +12,16 @@ tr_ip = {'data/data0203/dumplist_02_03/dumplist_'}; tr_flag = 0;
 % Legend nevek megadása:
 Scennames = {'Close infected classes';'Close infected schools';'Close all schools'};
 % Cím megadása:
-Title = 'Educational simulations';
+title = 'Educational simulations';
 % Plot határok bejelölése:
-Measures = {{'Restr.'},49;
+measures = {{'Restr.'},49;
             {'Vacci.'},96};
 % Kezdődátum megadása:
-StartD = '09/23/20'; % mm/dd/yy
+startd = '09/23/20'; % mm/dd/yy
 % Mentett grafikonok helye:
-Path = 'data/attempt/schooltest4';
+path = 'data/attempt/schooltest4';
 % Mérési adat flag:
 m_flag = 0;
 % Végül a függvény hívása:
-output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scennames,Title,Measures,StartD,std_flag,agst_flag,tr_flag,m_flag,Path);
-clearvars agst_ip Scennames std_ip Title tr_ip Measures StartD Path std_flag agst_flag tr_flag m_flag
+output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scennames,title,measures,startd,std_flag,agst_flag,tr_flag,m_flag,path);
+clearvars agst_ip Scennames std_ip title tr_ip measures startd path std_flag agst_flag tr_flag m_flag
