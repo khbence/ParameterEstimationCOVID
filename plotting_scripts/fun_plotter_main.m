9b6007a2af207d968d118d507ca538a892b2b359
gerge <gergely.horvath96@gmail.com>
2021-03-03 13:43:01 +0100

    Including the color setting into the main function call

--- plotting_scripts/fun_plotter_main.m
+++ plotting_scripts/fun_plotter_main.m
@@ -1,7 +1,7 @@
-function output = fun_plotter_main(std_ip,agst_ip,tr_ip,scen_names,Title,Measures,StartDate,std_flag,agst_flag,tr_flag,rdata_flag,Path)
+function output = fun_plotter_main(std_ip,agst_ip,tr_ip,scen_names,Title,Measures,StartDate,std_flag,agst_flag,tr_flag,rdata_flag,Path,colors)
 
-    fun_plotter_std(std_ip,scen_names,Title,Measures,StartDate,std_flag,rdata_flag,Path,'No. of Agents')
-    fun_plotter_std(std_ip,scen_names,Title,Measures,StartDate,std_flag,rdata_flag,Path,'Ratio to Total Population (%)')
+    fun_plotter_std(std_ip,scen_names,Title,Measures,StartDate,std_flag,rdata_flag,Path,'No. of Agents',colors)
+    fun_plotter_std(std_ip,scen_names,Title,Measures,StartDate,std_flag,rdata_flag,Path,'Ratio to Total Population (%)',colors)
     
     if agst_flag == 1
         locations = jsondecode(fileread('locations0.json'));
@@ -25,6 +25,6 @@ function output = fun_plotter_main(std_ip,agst_ip,tr_ip,scen_names,Title,Measure
         locations = [];
     end
     
-    output = fun_plotter_tr(tr_ip,agst_ip,scen_names,Title,Measures,StartDate,tr_flag,agents,locations,Path);
+    output = fun_plotter_tr(tr_ip,agst_ip,scen_names,Title,Measures,StartDate,tr_flag,agents,locations,Path,colors);
 
 end
