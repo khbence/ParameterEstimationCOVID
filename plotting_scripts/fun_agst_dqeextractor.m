226f2cd28f0eb72ec0b866653e7c9f010e6ec4ee
gerge <gergely.horvath96@gmail.com>
2021-03-07 18:28:36 +0100

    Nursing home bug fixed

--- plotting_scripts/fun_agst_dqeextractor.m
+++ plotting_scripts/fun_agst_dqeextractor.m
@@ -1,44 +1,60 @@
-function [deceased,quarantined,elders] = fun_agst_dqeextractor(AgentStat,agents)
+function output = fun_agst_dqeextractor(AgentStat,agents,clog,LocMap)
 
     p = 1;
-    q = 1;
-    r = 1;
     
     for i = 1 : length(AgentStat)
         
-        if strcmp(AgentStat(i).worstState_name,'D1')
-            deceased(p) = AgentStat(i);
-            p = p + 1;
-        end
-        
-        if AgentStat(i).daysInQuarantine ~= 0
-            quarantined(q) = AgentStat(i);
-            q = q + 1;
-        end
-        
-        booleanvar = 0;
-        
-        for j = 1 : length(agents(AgentStat(i).ID+1).locations)
-            if agents(AgentStat(i).ID+1).locations(j).typeID == 22
-                booleanvar = 1;
+        if clog == 1
+            if strcmp(AgentStat(i).worstState_name,'D1')
+                deceased(p) = AgentStat(i);
+                p = p + 1;
             end
-        end
-        
-        if booleanvar == 1
-            elders(r) = AgentStat(i);
-            r = r + 1;
+        elseif clog == 2
+            if AgentStat(i).daysInQuarantine ~= 0
+                quarantined(p) = AgentStat(i);
+                p = p + 1;
+            end
+        elseif clog == 3
+            booleanvar = 0;
+
+            for j = 1 : length(agents(AgentStat(i).ID+1).locations)
+                IDstring = agents(AgentStat(i).ID+1).locations(j).locID;
+                IDint = agents(AgentStat(i).ID+1).locations(j).typeID;
+                if IDint == 2
+                    LocStruct = LocMap(IDstring);
+                    if LocStruct.type == 22
+                        booleanvar = 1;
+                    end
+                end
+            end
+
+            if booleanvar == 1
+                elders(p) = AgentStat(i);
+                p = p + 1;
+            end
         end
         
     end
 
-    if exist('deceased','var') == 0
-        deceased = 0;
-    end
-    if exist('quarantined','var') == 0
-        quarantined = 0;
-    end
-    if exist('elders','var') == 0
-        elders = 0;
+    if clog == 1
+        if exist('deceased','var') == 0
+            deceased = 0;
+        end
+    elseif clog == 2
+        if exist('quarantined','var') == 0
+            quarantined = 0;
+        end
+    elseif clog == 3
+        if exist('elders','var') == 0
+            elders = 0;
+        end
     end
     
+    if clog == 1
+        output = deceased;
+    elseif clog == 2
+        output = quarantined;
+    elseif clog == 3
+        output = elders;
+    end
 end
