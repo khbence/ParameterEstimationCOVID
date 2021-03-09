1f2fa70ef6fa87a53b3c793e3674edb6f338a7f7
gerge <gergely.horvath96@gmail.com>
2021-03-09 10:43:22 +0100

    Bug fix in the splitter algorithm, handling the case if the given interval limit date does contain any infections

--- plotting_scripts/fun_agst_findFoLindex.m
+++ plotting_scripts/fun_agst_findFoLindex.m
@@ -2,28 +2,36 @@ function output = fun_agst_findFoLindex(agentstat,index,FoL)
 
     if FoL == 0
 
-        for i = 1 : length(agentstat)
+        while exist(output,'var')
+            for i = 1 : length(agentstat)
 
-            if funcutter(agentstat(i).infectionTime) == index
+                if funcutter(agentstat(i).infectionTime) == index
 
-                output = i;
-                break;
+                    output = i;
+                    break;
+
+                end
 
             end
-
+            
+            index = index + 1;
         end
         
     elseif FoL == 1
         
-        for i = 1 : length(agentstat)
+        while exist(output,'var')
+            for i = 1 : length(agentstat)
 
-            if funcutter(agentstat(i).infectionTime) == index && fun_tr_cutter(agentstat(i+1).infectionTime) ~= index
+                if funcutter(agentstat(i).infectionTime) == index && fun_tr_cutter(agentstat(i+1).infectionTime) ~= index
 
-                output = i;
-                break;
+                    output = i;
+                    break;
+
+                end
 
             end
-
+            
+            index = index - 1;
         end
         
     end
