fc4d204544287cfe0388060a2a15dc21eb39489d
gerge <gergely.horvath96@gmail.com>
2021-03-09 10:39:52 +0100

    Extending the function with the additional interval inputs

--- plotting_scripts/fun_plotter_agstorg.m
+++ plotting_scripts/fun_plotter_agstorg.m
@@ -1,18 +1,9 @@
-function fun_plotter_agstorg(agst_ip,scen_names,Title,agst_flag,agents,Path,LocMap)
-
-    if agst_flag == 1
-        
-        fun_agst_preread(agst_ip);
-        fun_plotter_agst(agst_ip,scen_names,Title,agents,0,'general',Path,LocMap)
-        fun_plotter_agst(agst_ip,scen_names,Title,agents,1,'deceased portion',Path,LocMap)
-        fun_plotter_agst(agst_ip,scen_names,Title,agents,2,'quarantined portion',Path,LocMap)
-        fun_plotter_agst(agst_ip,scen_names,Title,agents,3,'pensioner portion',Path,LocMap)
-        fun_plotter_agst(agst_ip,scen_names,Title,agents,4,'hospitalized portion',Path,LocMap)
+function fun_plotter_agstorg(agst_ip,scen_names,Title,agents,Path,LocMap,runflag,interval)
     
-    elseif agst_flag == 0
-        fprintf("Agentstat input processing was not requested!\n")
-    else
-        fprintf("You used something other than 0 or 1 for a binary flag!\n")
-    end
+    fun_plotter_agst(agst_ip,scen_names,Title,agents,0,'general',Path,LocMap,runflag,interval)
+    fun_plotter_agst(agst_ip,scen_names,Title,agents,1,'deceased portion',Path,LocMap,runflag,interval)
+    fun_plotter_agst(agst_ip,scen_names,Title,agents,2,'quarantined portion',Path,LocMap,runflag,interval)
+    fun_plotter_agst(agst_ip,scen_names,Title,agents,3,'pensioner portion',Path,LocMap,runflag,interval)
+    fun_plotter_agst(agst_ip,scen_names,Title,agents,4,'hospitalized portion',Path,LocMap,runflag,interval)
 
 end
