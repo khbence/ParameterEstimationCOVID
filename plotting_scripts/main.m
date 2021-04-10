177c8021cc63f3c86dedc1fc32e68d23eb3d3cba
gerge <gergely.horvath96@gmail.com>
2021-04-10 20:07:35 +0200

    sample main.m update

--- plotting_scripts/main.m
+++ plotting_scripts/main.m
@@ -1,25 +1,21 @@
 close all; clear all; clc;
 % Standard input:
-std_ip = {'data/agtyptest/agentType_0302_test_',
-          'data/agtyptest/aagentType_0305_test_',
-          'data/agtyptest/agentType_0305_test_testingP_50percent_higher_',
-          'data/agtyptest/agentType_0305_test_testingP_100percent_higher_',
-          'data/agtyptest/agentType_0305_test_testingP_150percent_higher_'}; std_flag = 1;
+std_ip = {'data/defdata/default_'}; std_flag = 1;
 % Agentstat input:
-agst_ip = {'data/fullRerun/default/outAgent/default_outagent_'}; agst_flag = 0;
+agst_ip = {'data/defdata/outAgent/default_'}; agst_flag = 0;
 % Trace input:
 tr_ip = {'data/data0203/dumplist_02_03/dumplist_'}; tr_flag = 0;
 % Legend nevek megadása:
-Scennames = {'03.02.';'03.05. with default testingP';'03.05. with 50% higher testingP';'03.05. with 100% higher testingP';'03.05. with 150% higher testingP'};
+Scennames = {'Default'};
 % Cím megadása:
-title = 'Agenttype test';
+title = 'Scenarios';
 % Plot határok bejelölése:
 measures = {{'Restr.'},49;
             {'Vacci.'},100};
 % Kezdődátum megadása:
 startd = '10/01/20'; % mm/dd/yy
 % Mentett grafikonok helye:
-path = 'data/agenttest';
+path = 'data/result_default/default_1';
 % Mérési adat flag:
 m_flag = 1;
 % Színek:
@@ -28,6 +24,10 @@ colors = [[255 0 0];[0 255 0];[0 0 255];[0 0 0];[255 0 255];[0 255 255]];
 intervals = [0 100; 101 200; 201 300];
 % Idősor ábrázolási kezdet:
 begintint = 8;
+% Standard statisztika nE/aI idősor súlyozás flag:
+ctw_flag = 1;
 % Végül a függvény hívása:
-output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scennames,title,measures,startd,std_flag,agst_flag,tr_flag,m_flag,path,colors,intervals,begintint);
-clearvars agst_ip Scennames std_ip title tr_ip measures startd path std_flag agst_flag tr_flag m_flag colors intervals begintint
+output = fun_plotter_main(std_ip,agst_ip,tr_ip,Scennames,title,measures,startd,std_flag,agst_flag,tr_flag,...
+                          m_flag,path,colors,intervals,begintint,ctw_flag);
+clearvars agst_ip Scennames std_ip title tr_ip measures startd path std_flag agst_flag tr_flag
+clearvars m_flag colors intervals begintint
